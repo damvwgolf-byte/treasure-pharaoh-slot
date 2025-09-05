df18cb5b03f00b3d4c891dbd3db9d9c9a1bdfbbf
damvwgolf-byte <damvwgolf@gmail.com>
2025-09-06 00:39:22 +0200

    Update main.jsx

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+// ATTENTION : ton App.jsx est dans /source/src
+import App from "../../source/src/App.jsx";
+// et ton index.css est dans /source
+import "../../source/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
